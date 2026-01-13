df940d1833fd3ef9cd35818bae21bb058bb10d2e
abdinasir13 <61417745+abdinasir13@users.noreply.github.com>
2026-01-12 22:22:00 -0500

    Initialize App component with ShortsAutopilot

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ShortsAutopilot from './components/ShortsAutopilot';
+
+function App() {
+  return (
+    <div className="App">
+      <ShortsAutopilot />
+    </div>
+  );
+}
+
+export default App;
